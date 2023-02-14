1009bb64fb2d37e05be9de4a9e8b5d21f909d3af
ashadeep <ashadeep746@gmail.com>
2023-02-15 05:21:37 +0530

    security1

---
+++ src/security/security.java
@@ -0,0 +1,13 @@
+package security;
+
+public  class security {
+    public double d12 = 1243;
+    public double d13 = 90980.32;
+
+    public int security() {
+        d12= 123.12;
+        int i = 12;
+        double security1 = (int) (d12 * i);
+        return (int) security1;
+    }
+}
---
+++ src/security1.java
@@ -0,0 +1,20 @@
+import security.security;
+
+public class security1 {
+     static security security = new security();
+
+    private int security1() {
+        int balance =10;
+        return balance;
+    }
+
+    public static void main(String[] args) {
+        security1 securityobj= new security1();
+        security security = new security();
+        System.out.println(securityobj.security1());
+         System.out.println(security.security());
+
+    }
+
+
+}
